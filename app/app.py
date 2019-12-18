b91ab7042635204cbce2e32ab28c22db34a32683
Maxon Tayebwa <maxtayebwa@gmail.com>
2019-12-18 22:15:52 +0300

    feat: add .flaskenv file to enable use of flask command + modify app.py to enable the use of the python command to run the app too

--- app/app.py
+++ app/app.py
@@ -1,12 +1,11 @@
 from flask import Flask
 
 app = Flask(__name__)
-app.debug = True
 
 
 @app.route('/')
 def home():
-    return "Hello World!"
+    return "Hello World!!"
 
-
-app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
