fa5ae2cdf443f37cc0c80fea5f9d1ef2eb94ba11
Maxon Tayebwa <maxtayebwa@gmail.com>
2019-12-19 17:02:27 +0300

    feature: add jwt authentication

--- flask-restful-docker/app.py
+++ flask-restful-docker/app.py
@@ -1,13 +1,23 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+from os import environ
+
+from security import authenticate, identity
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = environ['JWT_SECRET']
+app.config['JWT_AUTH_URL_RULE'] = '/login'
+
 api = Api(app)
+
+jwt = JWT(app, authenticate, identity)
 
 items = []
 
 
 class Item(Resource):
+    @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x['name'] == name, items), None)
         return {'item': item}, 200 if item else 404
