79c6e31d9eba26be8961f5d857c5c4dd9678f59f
Maxon Tayebwa <maxtayebwa@gmail.com>
2019-12-19 14:51:48 +0300

    feat: add item resource -create, view and view one item

--- flask-restful-docker/app.py
+++ flask-restful-docker/app.py
@@ -1,15 +1,35 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
+items = []
 
-class Student(Resource):
+
+class Item(Resource):
     def get(self, name):
-        return {'student': name}
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        return {'item': item}, 200 if item else 404
+
+    def post(self, name):
+        if next(filter(lambda x: x['name'] == name, items), None):
+            return {'message': f'An item with name {name} exists already'}, 400
+
+        data = request.get_json()
+        item = {'name': name, 'price': data['price']}
+        items.append(item)
+        return item, 201
 
 
-api.add_resource(Student, '/student/<string:name>')
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
 
-app.run(port=5000)
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
