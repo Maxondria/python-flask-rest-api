ea465d9175aa852ce9381ad0964a2182e6f71bdb
Maxon Tayebwa <maxtayebwa@gmail.com>
2019-12-28 16:54:10 +0300

    feat: register new user, modify docker* files

---
+++ flask-db-docker/app.py
@@ -0,0 +1,70 @@
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
+from flask_jwt import JWT, jwt_required
+from os import environ
+
+from security import authenticate, identity
+from user import UserRegister
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = environ['JWT_SECRET']
+
+api = Api(app)
+
+jwt = JWT(app, authenticate, identity)
+
+items = []
+
+
+class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+                        type=float,
+                        required=True,
+                        help='Price is required!!'
+                        )
+
+    @jwt_required()
+    def get(self, name):
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        return {'item': item}, 200 if item else 404
+
+    def post(self, name):
+        if next(filter(lambda x: x['name'] == name, items), None):
+            return {'message': f'An item with name {name} exists already'}, 400
+
+        data = Item.parser.parse_args()
+
+        item = {'name': name, 'price': data['price']}
+        items.append(item)
+        return item, 201
+
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return {'message': 'Item deleted'}
+
+    def put(self, name):
+        data = Item.parser.parse_args()
+
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
+
+
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
+
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+api.add_resource(UserRegister, '/register')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
