4da168dfcb9859442f503a1ff2f1642e99a10b5f
Maxon Tayebwa <maxtayebwa@gmail.com>
2020-01-09 18:03:20 +0300

    refc: config and refactor to use SQLAchemy in user model

--- flask-sqlalchemy/resources/user.py
+++ flask-sqlalchemy/resources/user.py
@@ -1,6 +1,7 @@
 import sqlite3
 from flask_restful import Resource, reqparse
 from models.user import UserModel
+from db import db
 
 
 class UserRegister(Resource):
@@ -22,13 +23,7 @@ class UserRegister(Resource):
         if UserModel.find_by_username(data['username']):
             return {'message': f"{data['username']} exists already"}, 400
 
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "INSERT INTO users VALUES (NULL, ?, ?)"
-        cursor.execute(query, (data['username'], data['password']))
-
-        connection.commit()
-        connection.close()
+        user = UserModel(**data)
+        user.save_to_db()
 
         return {'message': 'User created successfully'}, 201
