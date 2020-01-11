7dae0d954842313dba6f96f36a9b10a380cdbdcc
Maxon Tayebwa <maxtayebwa@gmail.com>
2020-01-11 15:01:51 +0300

    feat: required fresh jwt token on creation of items

--- flask-sqlalchemy/resources/item.py
+++ flask-sqlalchemy/resources/item.py
@@ -1,6 +1,11 @@
 from flask_restful import Resource, reqparse
 from models.item import ItemModel
-from flask_jwt_extended import jwt_required, jwt_optional, get_jwt_claims, get_jwt_identity
+from flask_jwt_extended import (
+    jwt_required,
+    jwt_optional,
+    get_jwt_claims,
+    fresh_jwt_required,
+    get_jwt_identity)
 
 
 class Item(Resource):
@@ -23,7 +28,7 @@ class Item(Resource):
             return item.json(), 200
         return {'message': f'An item with name {name} does not exist'}, 404
 
-    @jwt_required
+    @fresh_jwt_required
     def post(self, name):
         if ItemModel.find_by_name(name):
             return {'message': f'An item with name {name} exists already'}, 400
