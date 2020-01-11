f8e3c27617266207bb705f300b7c265fe9493b26
Maxon Tayebwa <maxtayebwa@gmail.com>
2020-01-11 14:49:28 +0300

    feat: add a token refresh resource

--- flask-sqlalchemy/resources/user.py
+++ flask-sqlalchemy/resources/user.py
@@ -1,6 +1,10 @@
 from flask_restful import Resource, reqparse
 from models.user import UserModel
-from flask_jwt_extended import create_access_token, create_refresh_token, jwt_required
+from flask_jwt_extended import (create_access_token,
+                                create_refresh_token,
+                                jwt_required,
+                                get_jwt_identity,
+                                jwt_refresh_token_required)
 
 _user_parser = reqparse.RequestParser()
 _user_parser.add_argument('username',
@@ -58,3 +62,11 @@ class UserLogin(Resource):
             refresh_token = create_refresh_token(user.id)
             return {'access_token': access_token, 'refresh_token': refresh_token}, 200
         return {'message': 'Invalid credentials provided'}, 401
+
+
+class TokenRefresh(Resource):
+    @jwt_refresh_token_required
+    def post(self):
+        current_user = get_jwt_identity()
+        new_token = create_access_token(identity=current_user, fresh=False)
+        return {'access_token': new_token}, 200
