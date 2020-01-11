535b9092e5b097a73157d47f7dccfb02ea34474e
Maxon Tayebwa <maxtayebwa@gmail.com>
2020-01-11 12:09:52 +0300

    feat: add user resource

--- flask-sqlalchemy/app.py
+++ flask-sqlalchemy/app.py
@@ -5,7 +5,7 @@ from os import environ
 from db import db
 
 from security import authenticate, identity
-from resources.user import UserRegister
+from resources.user import UserRegister, User
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
@@ -34,6 +34,7 @@ api.add_resource(Store, '/store/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
+api.add_resource(User, '/user/<int:user_id>')
 
 
 if __name__ == '__main__':
